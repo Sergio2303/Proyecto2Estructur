90bf1385b6fb1a4043e3b99475a6a6f067d00df1
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2023-05-29 18:49:05 -0600

    Creacion de ventana
    se creo la ventana con el logo

--- Login/forms/forms_master.py
+++ Login/forms/forms_master.py
@@ -2,20 +2,20 @@ import tkinter as tk
 from tkinter.font import BOLD
 import util.generic as utl
 
-class MasterPanel: 
+class MasterPanel:
     
-    #acá se modificaran las propiedades del titulo al igual que, ancho y alto de la ventana. 
-    def _init_(self): 
-        self.ventana = tk.TK()
+                                      
+    def __init__(self):        
+        self.ventana = tk.Tk()                             
         self.ventana.title('Master panel')
-        w, h = self.ventana.winfo_screenwidth(), self.ventana.winfo_screenheight()
+        w, h = self.ventana.winfo_screenwidth(), self.ventana.winfo_screenheight()                                    
         self.ventana.geometry("%dx%d+0+0" % (w, h))
-        self.ventana.config(bg = '#fcfcfc')
-        self.ventana.resizable(width=0, height=0)
-
-        logo =utl.leer_imagen("./imagenes/logo.png", (200, 200))
-
-        #colocar imagen en etiqueta de forma que ambas estnen centradas y de buen tamaño
+        self.ventana.config(bg='#fcfcfc')
+        self.ventana.resizable(width=0, height=0)            
         
-        label = tk.Label( self.ventana, image=logo,bg='##ffffff' )
+        logo =utl.leer_imagen("Login\imagenes\logo.jpg", (200, 200))
+                        
+        label = tk.Label( self.ventana, image=logo,bg='#3a7ff6' )
         label.place(x=0,y=0,relwidth=1, relheight=1)
+        
+        self.ventana.mainloop()
