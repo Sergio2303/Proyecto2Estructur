f0faeba4bb697ede3d519d6dd850bc3d993f111f
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2023-05-31 17:43:56 -0600

    login
    cambios recientes de login

--- Login/forms/form_login.py
+++ Login/forms/form_login.py
@@ -6,7 +6,7 @@ from forms.forms_master import MasterPanel
 
 class App:
 
-    def _init__(self):
+    def __init__(self):
         self.ventana = tk.Tk()
         self.ventana.title('Login')
         self.ventana.geometry('800x500') #tamaño del login 
@@ -14,6 +14,10 @@ class App:
         self.ventana.resizable(width=0, height=0)
         utl.centrar_ventana(self.ventana,800,500)
         self.ventana.mainloop()
+
+        logo =utl.leer_imagen("Login\imagenes\logo.jpg", (200, 200))
+        
+        #panel para el logo, acontinuacion se colocora el logo en una area en especifico. 
        
 
 
