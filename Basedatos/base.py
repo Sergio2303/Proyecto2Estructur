740e13bd072b9d6e248ef37aa419eb7951a0acc9
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2023-06-06 23:43:41 -0600

    Correcion y nuevas librerias
    Se colocaron nuevas librerias

--- Basedatos/base.py
+++ Basedatos/base.py
@@ -1,36 +1,8 @@
-from colorama import Fore, Back, Style
-import tkinter as tk
+#
+#
+#librerias
+#libreria para conectarse con la base de datos
+from neo4j import GraphDatabase 
+#libreria para ventana en python
+from tkinter import *
 from tkinter import messagebox
-from neo4j import GraphDatabase
-
-#PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-
-#Conexión a la base de datos Neo4j
-driver = GraphDatabase.driver("neo4j+s://2b702678.databases.neo4j.io", auth=("neo4j", "WR5R6xVto1O91dI_Y9prNnro8rvMR6swaeFmZHMaNVY"))
-# Crear una instancia de la ventana principal
-root = tk.Tk()
-root.withdraw()  # Ocultar la ventana principal
-
-#Obtencion de los grafos
-def obtener_grafo_genero():
-    with driver.session() as session:
-        result = session.run(
-            "MATCH (genero:Genero)<-[r]-(Canciones) "
-            "RETURN genero, type(r), Canciones"
-        )
-        #Guardar los datos mediante un ciclo
-        for record in result:
-            genero_node = record["genero"]
-            related_node = record["Canciones"]
-            #Muestra de resultados
-            print(Fore.WHITE +Back.BLACK +f"Género: {genero_node['Genero']}")
-            print(Fore.BLUE +Back.BLACK +f"Nodo Relacionado: {related_node}\n")
-           
-       
-
-#Ejemplo de uso
-obtener_grafo_genero()
-root.destroy()
-
-#Cerrar la conexión al finalizar
-driver.close()
